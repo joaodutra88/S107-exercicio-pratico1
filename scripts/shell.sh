c2f971c131d168bc4fa0adf36bc2fb3867ded24d
joaodutra88 <jvitordutra@gmail.com>
2023-04-10 14:22:41 -0300

    teste email

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -4,4 +4,4 @@ ls
 echo "Oi"
 sudo apt install mailutils
 echo "fim da instalacao"
-echo "Mandando e-mail com mail do linux" | mail -s "meu email deu tudo certo" jvitordutra@gmail.com
+echo "Mandando e-mail com mail do linux" | mail -s "meu email deu tudo certo" $EMAIL
