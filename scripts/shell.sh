a80c1273a6382ad3f0d0bf0f5f893665a1f1be55
joaodutra88 <jvitordutra@gmail.com>
2023-04-10 14:17:16 -0300

    att shell.sh

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -4,4 +4,4 @@ ls
 echo "Oi"
 sudo apt install mailutils
 echo "fim da instalacao"
-echo "Mandando e-mail com mail do linux" | mail -s "meu email deu tudo certo" $EMAIL
+echo "Mandando e-mail com mail do linux" | mail -s "meu email deu tudo certo" jvitordutra@gmail.com
